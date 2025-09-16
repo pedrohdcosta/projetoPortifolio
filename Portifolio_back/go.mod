fa6a6577bfb3159ff6e1b31bdc7367ccaa1ffae2
pedrohdcosta <pedrohdcosta@gmail.com>
2025-09-16 20:16:22 -0300

    criação da api

---
+++ Portifolio_back/go.mod
@@ -0,0 +1,3 @@
+module github.com/pedrohdcosta/go-api
+
+go 1.23.2
---
+++ Portifolio_back/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/pedrohdcosta/Portifolio_back/internal/http"
+)
